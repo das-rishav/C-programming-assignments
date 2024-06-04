c7e415533df754195c73644fad0616664220b13d
das-rishav <162004742+das-rishav@users.noreply.github.com>
2024-06-04 21:03:58 +0530

    Update a2_10.c

--- A2/a2_10.c
+++ A2/a2_10.c
@@ -1,3 +1,13 @@
+//Write a program to generate the following pattern: 
+//1
+//1 2 
+//1 2 3 
+//1 2 3 4 
+//1 2 3 4 5 
+//1 2 3 4 5 6 
+//1 2 3 4 5 6 7 
+//1 2 3 4 5 6 7 8 
+//1 2 3 4 5 6 7 8 9
 #include <stdio.h>
 
 int main() {
